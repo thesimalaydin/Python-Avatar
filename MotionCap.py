ca0d377e3cb995815a9f2dfd027f037126a09a70
thesimalaydin <simal.aydin@icloud.com>
2024-05-27 21:33:28 +0300

    first commit

---
+++ MotionCap.py
@@ -0,0 +1,25 @@
+import cv2
+from cvzone.PoseModule import PoseDetector
+
+cap = cv2.VideoCapture('squat.mp4')
+
+detector = PoseDetector()
+posList = []
+while True:
+    success, img = cap.read()
+    img = detector.findPose(img)
+    lmList, bboxInfo = detector.findPosition(img)
+
+    if bboxInfo:
+        lmString = ''
+        for lm in lmList:
+            lmString += f'{lm[0]},{img.shape[0] - lm[1]},{lm[2]},'
+        posList.append(lmString)
+
+    print(len(posList))
+
+    cv2.imshow("Image", img)
+    key = cv2.waitKey(1)
+    if key == ord('s'):
+        with open("AnimationFile.txt", 'w') as f:
+            f.writelines(["%s\n" % item for item in posList])
